77b114bc844083230dd8156b2f1de8e9a41a3b2a
eyal <eyal@leyal-desktop>
2008-03-10 22:21:13 +0200

    pytracefile.py now takes an output filename

--- tracer/pytracefile.py
+++ tracer/pytracefile.py
@@ -5,9 +5,25 @@ import os
 from pytracer import Tracer
 
 def main():
-    tracer = Tracer(open("profile.out", "wb"))
-    tracer_cmd = sys.argv.pop(0)
+    prog_name = sys.argv.pop(0)
+
+    output_filename = 'profile.out'
+    if sys.argv[0].startswith('-o'):
+        output_opt = sys.argv.pop(0)
+        if len(output_opt) > 2:
+            output_filename = output_opt[2:]
+        else:
+            output_filename = sys.argv.pop(0)
+    print "Tracing into %r" % (output_filename,)
+
+    assert not sys.argv[0].startswith('-'), "Unknown option %r" % (sys.argv[0],)
+
+    tracer = Tracer(open(output_filename, "wb"))
+
     python_program = sys.argv[0]
+    dirname = os.path.dirname(python_program)
+    sys.path.insert(0, dirname)
+
     g = {'__name__':'__main__'}
     tracer.trace(lambda : execfile(python_program, g, g))
 
